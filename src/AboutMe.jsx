26c3348b74614dc0262d0b411bd5b2d7da03affd
김예리 <dpfl0445@gmail.com>
2022-01-13 06:22:04 +0900

    AboutMe 구현 끝!

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -4,36 +4,70 @@ import Fade from "react-reveal/Fade";
 
 const AboutMe = () => {
     return (
-        <div>
-            <h1>About Me</h1>
-            <Fade bottom cascade>
-                <MeImg src="/img/fgfgg.jpg" />
-                <AboutMeMent>
-                    <span style={{ fontSize: 25 }}>
-                        접니다! 귀엽죠?
-                        <br />
-                    </span>
-                    <span style={{ fontSize: 20 }}>
-                        저는 어렸을적부터 만들기와 디자인을 좋아했습니다. <br />
-                        초등학교때 주전자 닷컴이라는 사이트에서 플래시게임을
-                        하다가 문득 직접 만들어보고 싶다는 생각이 들어
-                        플래시툴로 작은 미니게임을 만든게 제 개발 인생의
-                        시작이였던것같습니다.
-                        <br />그 후 성인이 되어 웹개발이 관심이 가 공부를
-                        시작했고, 어느새 여기까지 왔네요. 야호!
-                    </span>
-                </AboutMeMent>
-            </Fade>
-        </div>
+        <BodyOfAboutMe>
+            <h1 style={{ margin: "0 0 50px 20px" }}>About Me</h1>
+            <WrabbedAboutMe>
+                <Fade bottom cascade>
+                    <MeImg src="/img/me.png" />
+                    <AboutMeMent>
+                        <Title>
+                            아름다움을 프로그래밍하는 개발자
+                            <br />
+                        </Title>
+                        <Line />
+                        <Contents>
+                            저는 늘 시각적인 요소에 매력을 느껴왔습니다.
+                            <br />
+                            그 시작은 그저 사진을 이쁘게 꾸미는 정도에
+                            불과했지만, 애플 웹사이트를 보고 난 후부터 웹이라는
+                            세계에 완전히 빠져들게 되었고 이를 계기로 웹
+                            프론트엔드 개발 공부를 시작하게 되었습니다.
+                            <br />
+                            하고 싶은 것도 많고 욕심도 많아서 밤을 새가며 코드를
+                            업데이트하기도 했습니다.
+                            <br />
+                            어제보다 더 나은 사람이 되기위해 1일 1커밋을
+                            실천중입니다.
+                            <br />
+                            사용자에게 아름다운 경험을 선물하는 웹 프론트엔드
+                            개발자가 되고싶습니다.
+                            <br />
+                        </Contents>
+                    </AboutMeMent>
+                </Fade>
+            </WrabbedAboutMe>
+        </BodyOfAboutMe>
     );
 };
-
+const BodyOfAboutMe = styled.div`
+    margin: 150px 0 0 50px;
+`;
+const WrabbedAboutMe = styled.div`
+    display: flex;
+    height: 410px;
+    align-items: center;
+`;
 const MeImg = styled.img`
-    display: inline-block;
+    width: 300px;
+`;
+const Title = styled.span`
+    font-weight: 900;
+    font-size: 25px;
+`;
+
+const Line = styled.div`
+    width: 53%;
+    height: 1px;
+    background-color: black;
+    margin: 20px 0;
+`;
+const Contents = styled.span`
+    margin-top: 10px;
+    font-size: 20px;
 `;
 
 const AboutMeMent = styled.div`
-    display: inline-block;
     width: 800px;
+    margin-left: 50px;
 `;
 export default AboutMe;
