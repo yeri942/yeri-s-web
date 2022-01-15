47f40a0ca6362ad82f5e851df1f29f2229ec230f
김예리 <dpfl0445@gmail.com>
2022-01-16 03:23:33 +0900

    구름 페이드

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,43 +5,47 @@ import Fade from "react-reveal/Fade";
 const Main = () => {
     return (
         <MainWrab>
+            <Fade>
+                <Cloud1 src="/img/cloud1.png"></Cloud1>
+                <Cloud2 src="/img/cloud2.png"></Cloud2>
+            </Fade>
             <Fade bottom cascade>
                 <div style={{ paddingTop: "28vh" }}>
                     <Mainment>안녕하세요</Mainment>
-                    <Mainment>멋쟁이 개발자</Mainment>
+                    <br />
+                    <Mainment>프론트엔드 개발자</Mainment>
+                    <br />
                     <Mainment>김예리입니다.</Mainment>
                 </div>
             </Fade>
-            <Cloud1 src="/img/cloud1.png"></Cloud1>
-            <Cloud2 src="/img/cloud2.png"></Cloud2>
         </MainWrab>
     );
 };
 
 const MainWrab = styled.div`
+    position: relative;
     top: 0;
     background-color: lightblue;
-    min-width: 100%;
-    min-height: 100vh;
-    background-size: cover;
+    width: 100%;
+    height: 100vh;
     text-align: center;
 `;
+
 const Mainment = styled.div`
-    color: white;
+    display: inline-block;
+    color: rgb(43, 66, 109);
     font-size: 90px;
 `;
 const Cloud1 = styled.img`
     position: absolute;
-    right: 280px;
+    right: 15%;
     top: 430px;
-    height: 200px;
-    z-index: 200;
+    height: 250px;
 `;
 const Cloud2 = styled.img`
     position: absolute;
-    left: 280px;
+    left: 18%;
     top: 250px;
     height: 200px;
-    z-index: 200;
 `;
 export default Main;
