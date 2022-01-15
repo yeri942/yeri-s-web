46c6047d6a1dc9e9bb119e4c464be0d406a3c7ba
김예리 <dpfl0445@gmail.com>
2022-01-16 04:35:04 +0900

    stacks부분 뼈대만들기

--- src/Stacks.jsx
+++ src/Stacks.jsx
@@ -4,11 +4,34 @@ import Fade from "react-reveal/Fade";
 
 const Stacks = () => {
     return (
-        <div>
-            <h1>Stacks</h1>
-            <div>HTML,CSS,Js,React</div>
-        </div>
+        <WrabbedStack>
+            <h1
+                style={{
+                    display: "inline",
+                    backgroundColor: "rgba(255, 255, 255, 0.8)",
+                }}
+            >
+                Stacks
+            </h1>
+            <div>
+                <Circle />
+                <div>HTML,CSS,Js,React</div>
+            </div>
+        </WrabbedStack>
     );
 };
-
+const WrabbedStack = styled.div`
+    text-align: right;
+    margin: 300px 50px 0 0;
+`;
+const Circle = styled.div`
+    position: absolute;
+    width: 700px;
+    height: 700px;
+    right: -180px;
+    top: 1550px;
+    border-radius: 50%;
+    background-color: rgb(204, 232, 241);
+    z-index: -100;
+`;
 export default Stacks;
